4920f77c72715f5890b5c040f670e97cbd735f45
movlish <87419835+movlish@users.noreply.github.com>
2021-07-17 16:18:43 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ bot.on("message", async ctx => {
     const msg = ctx.message.text.toLowerCase()
 
     if (msg.includes(`шнк`)) {
-//         ctx.replyWithHTML(`ШНК (SHNQ SHaharsozlik Norma va Qoidalar) bilan tanishing`)
+//         ctx.replyWithHTML(`<B>O'ZBEKISTON RESPUBLIKASI QURILISH VAZIRLIGI </B>\n (<b>ШНК</b> - SHaharsozlik Norma va Qoidalar)\n(<b>КМК</b> - Qurilish Me'yorlari va Qoidalari) bilan tanishing`)
         sendStarMessage(ctx);
     }
     if (msg.includes(`штукатурка`)) {
@@ -196,6 +196,12 @@ bot.on("message", async ctx => {
     }
     else if (msg.includes('смонт')) {
         ctx.replyWithHTML(`<a href="https://t.me/shnkuz/54">Скачать Образец</a> АКТ смонтированного оборудования, инвентаря и материалов.\n<a href="https://t.me/shnkuz/51">Скачать</a> ВЕДОМОСТЬ смонтированного оборудования\n<a href="https://t.me/shnkuz/52">Скачать Образец</a> ВЕДОМОСТЬ смонтированного оборудования\n<a href="https://t.me/shnkuz/56">Скачать ШНК 3.01.04-04</a> ПРИЕМКА В ЭКСПЛУАТАЦИЮ ЗАКОНЧЕННЫХ СТРОИТЕЛЬСТВОМ ОБЪЕКТОВ`)
+    }
+    else if (msg.includes('Пропис')) {
+        ctx.replyWithHTML(`<a href="http://www.summa-propisyu.ru/?summ=269866635&vat=20&val=10&sep=0">Havolaga o'tish</a> Сумма Прописью Онлайн - сервис быстрого перевода`)
+    }
+    else if (msg.includes('Калькулятор')) {
+        ctx.replyWithHTML(`<a href="https://www.calc.ru/kalkulyator-rascheta-kirpicha-onlayn.html">Havolaga o'tish</a> Калькулятор расчета кирпича онлайн\n<a href="https://metallicheckiy-portal.ru/calculator/calculator_metalla">Havolaga o'tish</a> Калькулятор металла онлайн\n<a href="http://metsi.ru/calc">Havolaga o'tish</a> Калькулятор металлопроката онлайн\n<a href="http://compositgroup74.ru/kalkulyator.html">Havolaga o'tish</a> Онлайн калькулятор расчета размеров, арматуры и количества бетона монолитного ленточного фундамента`)
     }
     else {ctx.reply(`Bu haqida ma'lumot topilmadi.\nIltimos kirilchada yoki ruschada bexato yozib ko'ring!`)}
 })
