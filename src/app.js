3751655d6fb51c4debba45e3440ca39552e3cfc0
movlish13 <86656953+movlish13@users.noreply.github.com>
2021-08-23 17:57:47 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -78,6 +78,9 @@ bot.on("message", async ctx => {
 //         ctx.replyWithHTML(`<B>O'ZBEKISTON RESPUBLIKASI QURILISH VAZIRLIGI </B>\n (<b>ШНК</b> - SHaharsozlik Norma va Qoidalar)\n(<b>КМК</b> - Qurilish Me'yorlari va Qoidalari) bilan tanishing`)
         return sendStarMessage(ctx);
     }
+    else if (msg.includes(`штроб`)) {
+        return ctx.reply(`Е46-3-11`);
+    }
     else if (msg.includes(`штукатурка`)) {
         return ctx.reply(`Е61 ШТУКАТУРНЫЕ РАБОТЫ\nЕ15-2-1-1 Улучшенная штукатурка стен.\nЕ15-2-31-1 Штукатурка откосов.\nЕ15-2-36-1 Штукатурка по Сетке стен.\nВнутренний отделка.\nE15-2-31-1 Штукатурка откосов.\nЕ15-2-16-5 Штукатурка высококачественные стен.\nE46-2-9-2	ОТБИВКА ШТУКАТУРКИ С ПОВЕРХНОСТЕЙ СТЕН И ПОТОЛКОВ КИРПИЧНЫХ`)
     }
